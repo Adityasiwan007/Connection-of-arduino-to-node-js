1f2a1273e8f104cbc28b13ca00392606e3b95e6d
Adii <24523301+Adityasiwan007@users.noreply.github.com>
2019-04-12 11:16:44 +0530

    Update sketch_mar07a.ino

--- sketch_mar07a/sketch_mar07a.ino
+++ sketch_mar07a/sketch_mar07a.ino
@@ -1,54 +1,24 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
+const int trigPin = 9;
+const int echoPin = 10;
 
-const char* ssid = "Mi5";
-const char* password =  "tcs#1234";
-const char* mqttServer = "192.168.43.20";
-const int mqttPort = 1446;
-const char* mqttUser = "";
-const char* mqttPassword = "";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
+float duration, distance;
 
 void setup() {
-  Serial.begin(115200);         
-  delay(10);
-  Serial.println('\n');
-  
-  WiFi.begin(ssid, password);   
- 
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
-  }
-  
-  Serial.print(ssid);
-
- client.setServer(mqttServer, mqttPort);
- 
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-
-    if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
-
-      Serial.println("connected");
-
-    } else {
-
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
-
-    }
-  }
- client.publish("esp/test", "Hello from ESP32");
-
-  Serial.println(WiFi.localIP());         
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
- 
+
 void loop() {
-  client.loop();
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(100);
 }
